698fe17cf927ad2d6bd9bd18b167f84e7ce49479
Mubarak Ibrahim <mubarakibrahim2015@gmail.com>
2024-08-24 00:03:49 +0100

    feat: made login integration and validations

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-import {extractCookies} from '@shipex/utils';
+import {extractCookies, handleError} from '@shipex/utils';
 import {LoginResponse} from '@shipex/utils/types';
 import axios from 'axios';
 
@@ -12,26 +12,16 @@ const axiosInstance = axios.create({
   },
 });
 
-// export const fetcher = (url: string, data?: any) =>
-//   axiosInstance.post(url, data).then(res => {
-//     extractCookies(res);
-//     return res.data;
-//   });
-
 export const fetcher = async <T = any>(url: string, data?: any): Promise<T> => {
   try {
     const response = await axiosInstance.post(url, data);
     extractCookies(response);
-    // Do something with cookies if needed
-    //   console.log(cookies);
+
     return response.data;
   } catch (error) {
-    // Handle errors accordingly
-    console.error('Fetcher error:', error);
-    throw error;
+    handleError(error);
   }
 };
 
-// Use `fetcher` with appropriate type for responses
 export const loginFetcher = (data: {usr: string; pwd: string}) =>
   fetcher<LoginResponse>('/login', data);
